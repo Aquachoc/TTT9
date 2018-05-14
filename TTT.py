93776410a6395d2e94b6e0878378b07b1652d6b8
Aquachoc <aquachoc97@gmail.com>
2018-05-14 03:25:14 +0200

    Add cute octocat story

--- TTT.py
+++ TTT.py
@@ -6,9 +6,11 @@ Created on Tue Mar 27 15:31:16 2018
 from tkinter import *
 import numpy as np
 from random import random
+import math
 x=0
 y=0
-
+mut_range=6
+random_range=10
 def bToG(grid): #binaire -> tableau
     G=[]
     L=[] #grille annexe
@@ -92,18 +94,18 @@ def gprint(grid):
         for l in range(3):
             for i in range(3):
                 for j in range(3):
-                    if grid[k][l][i][j]==1:
+                    if grid[k][l][i][j]==2:
                         circle(w,k,l,i,j)
                         print("circle",i,j,k,l,'\n')
-                    elif grid[k][l][i][j]==2:
+                    elif grid[k][l][i][j]==1:
                         cross(w,k,l,i,j)
                         print("cross",i,j,k,l,'\n')
     metaGrid=winGrid(grid)
     for i in range(3):
         for j in range(3):
-            if metaGrid[i][j]==1:
+            if metaGrid[i][j]==2:
                 bigCircle(w,i,j)
-            elif metaGrid[i][j]==2:
+            elif metaGrid[i][j]==1:
                 bigCross(w,i,j)
             
     
@@ -168,7 +170,7 @@ def possible(grid, last, mgrid=[]):
         n=len(a[0])
         b=zip(last[0]*np.ones(n,dtype=int),last[1]*np.ones(n,dtype=int),a[0],a[1])
         b= list(b)
-    if mgrid!=[]:    
+    if len(mgrid)>0:    
         k=0
         while k<len(b):
             if mgrid[b[k][0]][b[k][1]]:
@@ -194,14 +196,21 @@ class Game:
         self.gagnable=np.ones((2,3,3),dtype=int)
         self.p1.tmpId=1
         self.p2.tmpId=2
+        self.p1.gAct=self.p2.gAct=self
+        self.turn=-1
+        self.mHist=np.zeros((3,3),dtype=int)
     def move(self,m):
         i,j,k,l=m
         vict=0
+        histM=0
         if not self.grid[i][j][k][l]:
             if (self.last==[i,j] or self.last==[3,3]): # and self.grid[i][j][k][l]==0
                 self.grid[i][j][k][l]=self.actual
                 self.last=[k,l]
-                self.mgrid=winGrid(self.grid)
+                histM=isWon(self.grid[i][j])
+                self.mgrid[i][j]=histM
+                if histM>0:
+                    self.mHist[i][j]=self.turn
                 vict=isWon(self.mgrid)              #grosse amélioration possible
                 if vict:
                     self.winner=vict
@@ -213,11 +222,12 @@ class Game:
                 
     def auto_play(self):
         while self.winner==0:
+            self.turn+=1
             if self.actual==1:
-                m=self.p1.move(self.grid,self.last)
+                m=self.p1.move()
                 
             else:
-                m=self.p2.move(self.grid,self.last)
+                m=self.p2.move()
             if m!=None:
                 self.move(m)
                 self.hist.append(m)
@@ -234,13 +244,17 @@ class Game:
             self.p2.score+=0.5
 
 class Player:
-    def __init__(self,pond=[],name=0):
+    name=0
+    def __init__(self,pond=[3,2,1,1,1,1,1,1,1,1,1, math.inf],typ="normal"):
         self.score=0
-        self.name=name
-        self.pond=pond
-        
-    def move1(self,grid,last):
-        poss=possible(grid,last)
+        self.name=Player.name
+        Player.name+=1
+        self.pond=np.array(pond)
+        self.tmpId=0
+        self.gAct=None
+        self.typ=typ
+    def move1(self,grid,last,mgrid):
+        poss=possible(grid,last,mgrid)
         n=len(poss)
         if not n:
             return None
@@ -248,35 +262,83 @@ class Player:
         k=int(k)
         return poss[k]
     
-    def move2(self,grid,last):
-        poss=possible(grid,last)
-        
+    def move2(self):
+        poss=possible(self.gAct.grid,self.gAct.last,self.gAct.mgrid)
+        i=-math.inf
+        n=len(poss)
+        M=None
+        s=0
+        if not n:
+            return None
+        for m in poss:
+            s=self.eurist(m)
+            if s>i:
+                M=m
+                i=s
+        return M
     
     
-    def move(self,grid,last):
-        return self.move1(grid,last)
+    def move(self):
+        if self.typ=="dummy":
+            return self.move1(self.gAct.grid, self.gAct.last, self.gAct.mgrid)
+        return self.move2()
+    
+    def indicateurs(self,m):
+        g=self.gAct
+        k=m[0:2]
+        m=m[2:]
+        tmp=np.array(g.grid)
+        tmp[k[0]][k[1]][m[0]][m[1]]=self.tmpId
+        won=isWon(tmp[k[0]][k[1]])
+        adv_winnable=winnable(tmp[k[0]][k[1]], 1+self.tmpId%2)
+        mGrid=winGrid(tmp)
+        capture=(mGrid[k[0]][k[1]]==self.tmpId)*1
+        victory=isWon(mGrid)
+        return[1*center_flag(m), 1*corner_flag(m), 1*border_flag(m),  nbPions(g.grid[m[0]][m[1]],self.tmpId,m), nbPions(g.grid[m[0]][m[1]], 1+(self.tmpId%2), m), 1*(g.mgrid[m[0]][m[1]]!=0), 1*winnable(g.grid[m[0]][m[1]]), int(winnable(g.grid[m[0]][m[1]], 1+self.tmpId%2)),won, int(adv_winnable), capture, victory]
+    
+    def eurist(self,m):
+        flags=np.array(self.indicateurs(m))
+        if not flags[-1]:
+            score=sum(flags[:-1]*self.pond[:-1])
+        else:
+            score=sum(flags*self.pond)
+        return score
     
 def conti(event):
-    n=len(event.widget.hist)
+    n=len(event.widget.g.hist)
     c=event.widget.count
     if c%2==1 and c<=n:
-        h=event.widget.hist[c-1]
-        cross(event.widget,h[0],h[1],h[2],h[3])
+        h=event.widget.g.hist[c-1]
+        a=cross(event.widget,h[0],h[1],h[2],h[3])
+        event.widget.tmp.append(a)
+        if event.widget.g.mHist[h[0]][h[1]]==c-1:
+            event.widget.mTmp.append(bigCross(event.widget,h[0],h[1]))
+        else:
+            event.widget.mTmp.append(None)
         event.widget.count+=1
     else:
-        h=event.widget.hist[c-1]
-        circle(event.widget,h[0],h[1],h[2],h[3])
+        h=event.widget.g.hist[c-1]
+        event.widget.tmp.append(circle(event.widget,h[0],h[1],h[2],h[3]))
+        if event.widget.g.mHist[h[0]][h[1]]==c-1:
+            event.widget.mTmp.append(bigCircle(event.widget,h[0],h[1]))
+        else:
+            event.widget.mTmp.append(None)
         event.widget.count+=1
+    return 1
     
         
 #    def eva(grid,move)
-def gHist(hist):
+def gHist(game):
         root = Tk()
         w = Canvas(root, width=900, height=900)
         w.pack()
+        w.tmp=[]
+        w.mTmp=[]
         w.count=1
-        w.hist=hist
+        w.g=game
+        w.root=root
         w.bind("<Button-1>", conti)
+        w.bind("<Button-3>", prev)
         for i in range(1,9):
             if i%3:
                 w.create_line(0,i*100,900,i*100)
@@ -284,9 +346,21 @@ def gHist(hist):
             else:
                 w.create_line(0,i*100,900,i*100, fill = "red", width=5)
                 w.create_line(i*100,0,i*100,900, fill = "red", width=5)
-        root.mainloop()    
+        root.mainloop()  
+        print(len(w.tmp))
             
-            
+def prev(event):
+    n=len(event.widget.g.hist)
+    c=event.widget.count
+    if n>0:
+        event.widget.count-=1
+        if event.widget.mTmp[-1]!=None:
+            event.widget.delete(event.widget.mTmp[-1])
+            del(event.widget.mTmp[-1])
+        event.widget.delete(event.widget.tmp[-1])
+        del(event.widget.tmp[-1])
+    event.widget.root.update()
+    return 1
 
 def create_batch(n):
     L=[]
@@ -305,7 +379,7 @@ def reset_scores(players):
 def recap(players):
     n=len(players)
     for i in range(n):
-        print(players[i].score,i,"\n")
+        print(players[i].score,players[i].name,"\n")
         
 def tournament(players,typ='f'):
     if typ=='f':
@@ -328,7 +402,6 @@ def winnable(grid,player=0): #0=both
     if player :    
         g=np.array(grid)
         g[g==0]=player
-        print(g)
         a=(isWon(g)==player)
     if not player :
         player=1
@@ -339,11 +412,86 @@ def winnable(grid,player=0): #0=both
         g[g==0]=1+player%2
         
         b=(isWon(g)==1+player%2)
-        print(a,b)
         return a*b
     return a
 
+def corner_flag(m):
+    return((m in [[0,2],[2,0],[0,0],[2,2]]))
 
+def border_flag(m):
+    return((m in [[0,1],[1,0],[2,1],[1,2]]))
+
+def center_flag(m):
+    return (m == [1,1])
+
+def nbPions(grid, nbPlayer, m=[]):
+    if not len(m):
+        k=-1
+        for i in range(3):
+            for j in range(3):
+                k = max(k,nbPions(grid,nbPlayer, [i,j]))
         
-        
+    else:
+        k=0
+        for i in range(3):
+            for j in range(3):
+                if grid[i][j]==nbPlayer:
+                    k+=1
+    return k
+
+def randomize(players):
+    n=len(players[0].pond)-1
+    for i in players:
+        i.pond[:-1]=np.array([random_range*random()-random_range*0.5 for _ in range(n)])
+
+def mutate(players):
+    n=len(players)
+    k=n//2
+    podium(players)
+    reset_scores(players)
+    players[:k]=create_batch(k)
+    for i in range(k):
+        players[i].pond=players[-i].pond
+        mutation(players[i])
+
+
+def mutation(player):
+    n=len(player.pond)-1
+    a=np.array([mut_range*random()-0.5*mut_range for _ in range(n)])
+    player.pond[:-1]+=a
+
+def generation(n,N=[]):
+    if len(N)==0:
+        N=create_batch(n)
+        randomize(N)
+    dummy_tournament(N)
+    mutate(N)
+    return N
+
+def dummy_tournament(players,n=100):
+    b=Player(typ="dummy")
+    j=0
+    l=0
+    w=0
+    for i in players:
+       print(j)
+       j+=1
+       for k in range(n):
+            if l%(n//10):
+               print(l)
+            if n%2:
+                main=Game(i,b)
+            else:
+                main=Game(b,i)
+            main.auto_play()
+            if main.winner==i.tmpId:
+                w+=1
+    return w
     
+    
+a=Player()
+b=Player()
+main=Game(a,b)
+main.last=[0,1]
+
+
